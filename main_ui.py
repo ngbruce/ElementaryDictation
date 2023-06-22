ef82790d8cec16090ee4dc3bd27208d8bb4521b8
NgBruce <woobruce@163.com>
2023-06-22 17:22:53 +0800

    增加设置文件

--- main_ui.py
+++ main_ui.py
@@ -5,20 +5,64 @@ import tkinter as tk
 from tkinter import filedialog
 import threading
 from playsound import playsound
+import configparser
 
 global contents
+global rate, vol, spdFactorEn, spdFactorCN
+# 配置文件路径
+config_dir = 'config'
+config_path = os.path.join(config_dir, 'config.ini')
+# 检查是否存在config_dir路径，如果没有，就创建
+if not os.path.exists(config_dir):
+    os.mkdir(config_dir)
+
+# 保存设置
+def save_settings():
+    # 从 Entry 中获取数值并赋值给全局变量
+    global rate, vol, spdFactorEn, spdFactorCN
+    rate = int(rate_entry.get())
+    vol = float(vol_entry.get())
+    spdFactorEn = float(spdFactorEn_entry.get())
+    spdFactorCN = float(spdFactorCN_entry.get())
+    # 创建配置文件对象
+    config = configparser.ConfigParser()
+    # 添加节
+    config.add_section('settings')
+
+    # 添加配置项
+    config.set('settings', 'rate', str(rate))
+    config.set('settings', 'vol', str(vol))
+    config.set('settings', 'spdFactorEn', str(spdFactorEn))
+    config.set('settings', 'spdFactorCN', str(spdFactorCN))
+
+    # 写入配置文件
+    with open(config_path, 'w') as f:
+        config.write(f)
 
 
-def beep_task():
-    while True:
-        beep_event.wait()  # 等待事件
-        playsound(".\\sounds\\start-13691.mp3")
-        beep_event.clear()  # 重置事件状态
+# 加载设置
+def load_settings():
+    # 创建配置文件对象
+    config = configparser.ConfigParser()
 
+    # 读取配置文件
+    config.read(config_path)
 
-beep_event = threading.Event()  # 播放事件对象
-beep_thread = threading.Thread(target=beep_task, daemon=True)
-beep_thread.start()
+    # 读取配置项并赋值给全局变量和 Entry 中
+    global rate, vol, spdFactorEn, spdFactorCN
+    rate = int(config.get('settings', 'rate'))
+    vol = float(config.get('settings', 'vol'))
+    spdFactorEn = float(config.get('settings', 'spdFactorEn'))
+    spdFactorCN = float(config.get('settings', 'spdFactorCN'))
+
+    rate_entry.delete(0, tk.END)
+    rate_entry.insert(0, str(rate))
+    vol_entry.delete(0, tk.END)
+    vol_entry.insert(0, str(vol))
+    spdFactorEn_entry.delete(0, tk.END)
+    spdFactorEn_entry.insert(0, str(spdFactorEn))
+    spdFactorCN_entry.delete(0, tk.END)
+    spdFactorCN_entry.insert(0, str(spdFactorCN))
 
 
 def choose_file():
@@ -58,13 +102,17 @@ def countdown(pause_time):
         mins, secs = divmod(pause_time, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         countdown_text.set(timeformat)
-        beep_event.set()
+        # test_beep(2, 1)
+        if pause_time >= 3:
+            beep_event.set()
+        else:
+            beep_event2.set()
         time.sleep(1)
         pause_time -= 1
 
 
 def dictate():
-    global contents
+    global contents, rate, vol, spdFactorEn, spdFactorCN
     rate = int(rate_entry.get())
     vol = float(vol_entry.get())
     spdFactorEn = float(spdFactorEn_entry.get())
@@ -74,9 +122,11 @@ def dictate():
     engine.setProperty('volume', vol)  # 设置音量
     voices = engine.getProperty('voices')  # 获取当前语音的详细信息
     engine.setProperty('voice', voices[1].id)
+    test_beep(2, 1)
     engine.say("attention, dictation begins now")
     engine.runAndWait()
     engine.setProperty('voice', voices[0].id)
+    test_beep2(2, 1)
     engine.say("注意，听写开始")
     engine.runAndWait()
     engine.setProperty('voice', voices[1].id)
@@ -95,6 +145,7 @@ def dictate():
         engine.runAndWait()
         countdown(pause_time)
         main_list.itemconfig(i, fg="black", bg="grey")
+        time.sleep(1)  # 避免声音冲突
 
     engine.setProperty('voice', voices[0].id)
     engine.say("听写结束")
@@ -104,11 +155,16 @@ def dictate():
     engine.runAndWait()
     engine.stop()
 
-
+global thread_dictate
 def start_dictate():
-    t = threading.Thread(target=dictate)
-    t.start()
+    global thread_dictate
+    thread_dictate = threading.Thread(target=dictate, daemon=True)
+    thread_dictate.start()
 
+
+def kill_dictate():
+    global thread_dictate
+    thread_dictate.exit()    # 使用terminate()方法终止线程
 
 # 创建主窗口
 root = tk.Tk()
@@ -167,7 +223,13 @@ spdFactorCN_entry.insert(0, "1.0")
 
 # 创建开始按钮
 start_button = tk.Button(root, text="开始听写", command=start_dictate)
-start_button.pack(side="bottom")
+start_button.pack(side="left")
+start_button = tk.Button(root, text="中止听写", command=kill_dictate)
+start_button.pack(side="left")
+start_button = tk.Button(root, text="保存设置", command=save_settings)
+start_button.pack(side="right")
+start_button = tk.Button(root, text="载入设置", command=load_settings)
+start_button.pack(side="right")
 
 # 创建倒计时文本框
 countdown_frame = tk.Frame(root)
@@ -179,5 +241,50 @@ countdown_text.set("")
 countdown_entry = tk.Entry(countdown_frame, textvariable=countdown_text, state="readonly")
 countdown_entry.pack(side="left")
 
+
+# 发音事件
+def beep_task():
+    while True:
+        beep_event.wait()  # 等待事件
+        playsound(".\\sounds\\start-13691.mp3")
+        beep_event.clear()  # 重置事件状态
+
+
+beep_event = threading.Event()  # 播放事件对象
+beep_thread = threading.Thread(target=beep_task, daemon=True)
+beep_thread.start()
+
+
+# 发音事件2
+def beep_task2():
+    while True:
+        beep_event2.wait()  # 等待事件
+        playsound(".\\sounds\\stop-13692.mp3")
+        beep_event2.clear()  # 重置事件状态
+
+
+beep_event2 = threading.Event()  # 播放事件对象2
+beep_thread2 = threading.Thread(target=beep_task2, daemon=True)
+beep_thread2.start()
+
+
+# 发音测试
+def test_beep(cnt: int, interval: float):
+    for i in range(cnt):
+        beep_event.set()
+        time.sleep(interval)
+
+
+# 发音测试2
+def test_beep2(cnt: int, interval: float):
+    for i in range(cnt):
+        beep_event2.set()
+        time.sleep(interval)
+
+# 加载设置
+try:
+    load_settings()
+except Exception as e:
+    print("错误：", e)
 # 运行主循环
 root.mainloop()
